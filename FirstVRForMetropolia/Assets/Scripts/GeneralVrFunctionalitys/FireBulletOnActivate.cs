b37a69c2a31fd2ff7d39c922f43ab27ef0d577ac
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2022-10-22 10:32:55 +0300

    Basic functionalities for working VR-rig

---
+++ FirstVRForMetropolia/Assets/Scripts/GeneralVrFunctionalitys/FireBulletOnActivate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FireBulletOnActivate : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnPoint;
+    public float fireSpeed = 100;
+
+    private void Start()
+    {
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(FireBullet);
+    }
+
+    public void FireBullet(ActivateEventArgs arg)
+    {
+        GameObject spawnedBullet = Instantiate(bullet);
+        spawnedBullet.transform.position = spawnPoint.position;
+        spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed * Time.deltaTime;
+        Destroy(spawnedBullet, 5f);
+    }
+}
