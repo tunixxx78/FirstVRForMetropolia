aff74a3e5283bac6bd3e1ec9219b04ec02d503fa
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2022-10-26 16:47:47 +0300

    RayDistanceHold added

--- FirstVRForMetropolia/Assets/Scripts/GeneralVrFunctionalitys/ActivateGrapRay.cs
+++ FirstVRForMetropolia/Assets/Scripts/GeneralVrFunctionalitys/ActivateGrapRay.cs
@@ -8,13 +8,19 @@ public class ActivateGrapRay : MonoBehaviour
     public GameObject leftGrapRay;
     public GameObject rightGrapRay;
 
+    public GameObject rightGrapRayHold;
+
     public XRDirectInteractor leftDirectGrap;
     public XRDirectInteractor rightDirectGrap;
+
+    public XRRayInteractor rightRayHoldGrap;
 
 
     private void Update()
     {
         leftGrapRay.SetActive(leftDirectGrap.interactablesSelected.Count == 0);
         rightGrapRay.SetActive(rightDirectGrap.interactablesSelected.Count == 0);
+
+        rightGrapRayHold.SetActive(rightDirectGrap.interactablesSelected.Count == 0 || rightRayHoldGrap.interactablesSelected.Count == 0);
     }
 }
